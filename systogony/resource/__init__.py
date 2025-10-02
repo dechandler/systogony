799ca48b3c9de674b4a7573aa7690add995d977f
David Chandler <contact@dechandler.io>
2025-10-02 01:08:42 -0700

    split resource files with two resources

--- systogony/resource/__init__.py
+++ systogony/resource/__init__.py
@@ -1,8 +1,12 @@
 
-from .host import Host, Interface
+from .host import Host
+
+from .interface import Interface
 
 from .network import Network
 
-from .service import Service, ServiceInstance
+from .service import Service
+
+from .service_instance import ServiceInstance
 
 from .acl import Acl
